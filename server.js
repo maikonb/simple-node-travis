1f99bd28c3096fd21a82de9941f1179148d84a99
maikon <maikon@gmail.com>
2020-08-22 19:51:39 -0400

    add msg

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const msg = process.env.MESSAGE;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`${msg}`);
     await sleep(5000);
   }
 }
